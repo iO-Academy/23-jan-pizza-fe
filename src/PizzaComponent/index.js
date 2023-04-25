9d1f5a789d866c3d24dae5353ab371bea38c81af
barebarebears <nick.hackland@gmail.com>
2023-04-25 15:53:42 +0100

    added in onclick event listener to pizza image

--- src/PizzaComponent/index.js
+++ src/PizzaComponent/index.js
@@ -1,11 +1,15 @@
 import Pizza from './pizza.png'
 
+const handleClick = () => {
+
+}
+
 const PizzaComponent = () => {
     return (
         <div>
             <div className='bg-blue-200 h-screen flex justify-center items-center'>
                 <div className='group relative'>
-                    <img className='h-80' alt='pizza image' src={Pizza} />
+                    <img onClick={handleClick} className='h-80' alt='pizza image' src={Pizza} />
                     <div className='hidden bg-indigo-500 group-hover:block absolute top-0 left-0 text-white p-2 rounded-lg font-mono'>
                         Click here to spin the pizza and pick from two toppings!
                     </div>
@@ -16,8 +20,3 @@ const PizzaComponent = () => {
 }
 
 export default PizzaComponent
-
-
-
-
-
