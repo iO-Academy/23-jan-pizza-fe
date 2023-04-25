315010c02bf0c6157308030d67a554c345cd0c19
Steph-ski <stephaniejohns57@gmail.com>
2023-04-25 16:06:02 +0100

    updated hover message following code review

--- src/PizzaComponent/index.js
+++ src/PizzaComponent/index.js
@@ -11,7 +11,7 @@ const PizzaComponent = () => {
                 <div className='group relative'>
                     <img onClick={handleClick} className='h-80' alt='pizza image' src={Pizza} />
                     <div className='hidden bg-indigo-500 group-hover:block absolute top-0 left-0 text-white p-2 rounded-lg font-mono'>
-                        Click here to spin the pizza and pick from two toppings!
+                        Click here to spin the pizza to display two toppings!
                     </div>
                 </div>
             </div>
