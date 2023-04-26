dc2f349c8ed4a618292783dc39b38b055e622731
barebarebears <nick.hackland@gmail.com>
2023-04-26 10:08:23 +0100

    changed wording of message

--- src/PizzaComponent/index.js
+++ src/PizzaComponent/index.js
@@ -23,7 +23,7 @@ const PizzaComponent = () => {
                         src={Pizza}
                     />
                     <div className='hidden bg-indigo-500 group-hover:block absolute top-0 left-0 text-white p-2 rounded-lg font-mono'>
-                        Click here to spin the pizza to display two toppings!
+                        Click the pizza to display two toppings!
                     </div>
                 </div>
             </div>
