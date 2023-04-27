f3b4a838a4116fc8d21871576adc574993d827e4
Steph-ski <stephaniejohns57@gmail.com>
2023-04-27 14:42:55 +0100

    toppings images added

--- src/Images/Images.js
+++ src/Images/Images.js
@@ -1,11 +1,51 @@
 import Meatballs from './meatballs.jpeg'
 import Mozarella from './mozeralla.jpeg'
 import Mushrooms from './mushrooms.jpeg'
+import Olives from './olives.jpeg'
+import Onion from './onions.jpeg'
+import Parmesan from './parmesan.jpeg'
+import Peppers from './peppers.jpeg'
+import PlainChicken from './plain-chicken.jpeg'
+import Proscuitto from './prosciutto.jpeg'
+import PulledPork from './pulled-pork.jpeg'
+import QuornChicken from './quorn-chicken.jpeg'
+import Rocket from './rocket.jpeg'
+import SpicyBeef from './spicy-beef.jpeg'
+import Sweetcorn from './sweetcorn.jpeg'
+import TandoorChicken from './tandoori-chicken.jpeg'
+import Tomato from './tomato.jpeg'
+import Aubergine from './aubergine.jpeg'
+import Bacon from './bacon.jpeg'
+import BlueCheese from './blue-cheese.jpeg'
+import Chorizo from './chorizo.jpeg'
+import Courgettes from './courgettes.jpeg'
+import Gherkins from './gherkins.jpeg'
+import Jalapenos from './jalapenos.jpeg'
 
 const images = {
     Meatballs: Meatballs,
     Mozarella: Mozarella,
-    Mushrooms: Mushrooms
+    Mushrooms: Mushrooms,
+    Olives: Olives,
+    Onion: Onion,
+    Parmesan: Parmesan,
+    Peppers: Peppers,
+    PlainChicken: PlainChicken,
+    Proscuitto: Proscuitto,
+    PulledPork: PulledPork,
+    QuornChicken: QuornChicken,
+    Rocket: Rocket,
+    SpicyBeef: SpicyBeef,
+    Sweetcorn: Sweetcorn,
+    TandoorChicken: TandoorChicken,
+    Tomato: Tomato,
+    Aubergine: Aubergine,
+    Bacon: Bacon,
+    BlueCheese: BlueCheese,
+    Chorizo: Chorizo,
+    Courgettes: Courgettes,
+    Gherkins: Gherkins,
+    Jalapenos: Jalapenos
 }
 
 export default images
