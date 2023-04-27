75d33041c3934865d470bafbc29cefe0e69e9bee
barebarebears <nick.hackland@gmail.com>
2023-04-27 12:34:34 +0100

    images.js added but not yet complete - images showing up in modal correctly but random selection of toppings only working on refresh

---
+++ src/Images/Images.js
@@ -0,0 +1,11 @@
+import Meatballs from './meatballs.jpeg'
+import Mozarella from './mozeralla.jpeg'
+import Mushrooms from './mushrooms.jpeg'
+
+const images = {
+    Meatballs: Meatballs,
+    Mozarella: Mozarella,
+    Mushrooms: Mushrooms
+}
+
+export default images
--- src/ToppingComponent/index.js
+++ src/ToppingComponent/index.js
@@ -1,11 +1,14 @@
 
+
+import Images from '../Images/Images'
+
 const ToppingsComponent = ({topping}) => {
 
     return (
         <>
             <div className='m-4 h-80'>
                 <h3>{topping.name}</h3>
-                <img  className='h-40' src={topping.img}/>
+                <img  className='h-40' src={Images[topping.name]}/>
                 <p> Category: {topping.category}</p>
             </div>
         </>
