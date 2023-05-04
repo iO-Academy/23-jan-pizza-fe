a990faa0f782caa0a6301eb3ddf979ae2b66a108
Steph-ski <stephaniejohns57@gmail.com>
2023-05-04 10:36:31 +0100

    voting button added

--- src/ToppingComponent/index.js
+++ src/ToppingComponent/index.js
@@ -14,10 +14,14 @@ const category = () => {
 
     return (
         <>
-            <div className='m-4 h-80'>
+            <div className='flex-col flex justify-center items-center m-4 h-80'>
                 <h3 className='font-bold text-center pb-2'>{topping.name}</h3>
-                <img  className='h-40 w-40 object-fill' alt='toppings-img' src={Images[topping.img]}/>
+                <div>
+                    <img className='h-40 w-40 object-fit' alt='toppings-img' src={Images[topping.img]}/>
+                </div>
                 <p className='text-center pt-3'>{category(topping.category)}</p>
+                <button className='bg-cyan-950 transform: scale-50 hover:bg-red-800 text-white font-bold py-2 px-4
+                rounded-full border-white border-4 border-opacity-50 hover:border-black hover:border-opacity-100 shadow w-80 h-40 text-4xl'>Vote</button>
             </div>
         </>
     )
