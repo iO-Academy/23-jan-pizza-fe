fa148b9874219860881608e80b9d6770695d8682
Steph-ski <stephaniejohns57@gmail.com>
2023-05-04 13:15:48 +0100

    put requested added to link with BE & MongoDB

--- src/ToppingComponent/index.js
+++ src/ToppingComponent/index.js
@@ -12,6 +12,24 @@ const category = () => {
     }
 }
 
+
+    const addVoteToTopping = async () => {
+
+        try {
+            await fetch('http://localhost:9000/toppings', {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({id: topping._id})
+            }).then((response) => {
+                return response.json()
+            })
+        }
+     catch {
+        console.log('dont work')
+    }}
+
     return (
         <>
             <div className='flex-col flex justify-center items-center m-4 h-80'>
@@ -20,7 +38,7 @@ const category = () => {
                     <img className='h-40 w-40 object-fit' alt='toppings-img' src={Images[topping.img]}/>
                 </div>
                 <p className='text-center pt-3'>{category(topping.category)}</p>
-                <button className='bg-cyan-950 transform: scale-50 hover:bg-red-800 text-white font-bold py-2 px-4
+                <button onClick={addVoteToTopping} className='bg-cyan-950 transform: scale-50 hover:bg-red-800 text-white font-bold py-2 px-4
                 rounded-full border-white border-4 border-opacity-50 hover:border-black hover:border-opacity-100 shadow w-80 h-40 text-4xl'>Vote</button>
             </div>
         </>
